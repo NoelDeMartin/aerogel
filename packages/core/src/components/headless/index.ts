e4d0cfe885142c52d82ff3d83de3300d3f824444
Noel De Martin <noel@noeldemartin.com>
2023-06-05 19:54:53 +0200

    Fix import

--- packages/core/src/components/headless/index.ts
+++ packages/core/src/components/headless/index.ts
@@ -1,7 +1,7 @@
 import AGHeadlessModal from './AGHeadlessModal.vue';
 import AGHeadlessModalPanel from './AGHeadlessModalPanel.vue';
 import AGHeadlessModalTitle from './AGHeadlessModalTitle.vue';
-import type { IAGHeadlessModal } from '@/components/headless/AGHeadlessModal';
+import type { IAGHeadlessModal } from './AGHeadlessModal';
 
 export { AGHeadlessModal, AGHeadlessModalPanel, AGHeadlessModalTitle };
 export type { IAGHeadlessModal };
