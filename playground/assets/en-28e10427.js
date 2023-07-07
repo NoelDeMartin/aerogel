6ebf7faf6101419733f3181bc686c15e8d2cce48
Noel De Martin <noel@noeldemartin.com>
2023-07-07 12:34:52 +0200

    deploy

---
+++ playground/assets/en-28e10427.js
@@ -0,0 +1,16 @@
+const t={app:{title:e=>{const{normalize:n}=e;return n(["Aerogel Playground"])},viewPageSource:e=>{const{normalize:n}=e;return n(["View page source"])}},footer:{about:e=>{const{normalize:n}=e;return n(["about"])},source:e=>{const{normalize:n}=e;return n(["source"])}},home:{title:e=>{const{normalize:n}=e;return n(["Welcome to Aerogel!"])},description:e=>{const{normalize:n}=e;return n(["Explore these features:"])},content:e=>{const{normalize:n}=e;return n(["Formatted Content"])},forms:e=>{const{normalize:n}=e;return n(["Forms"])},modals:e=>{const{normalize:n}=e;return n(["Modals"])},storage:e=>{const{normalize:n}=e;return n(["Storage"])}},content:{title:e=>{const{normalize:n}=e;return n(["Formatted Content"])},body:e=>{const{normalize:n}=e;return n([`This is an example of how you can easily write formatted content with markdown!
+
+### Formatting
+
+You can use **bold**, *italic*, \`code\`, ...
+
+### Lists
+
+- One
+- Two
+- Three
+
+### Images
+
+![A cute kitten](http://placekitten.com/300/300)
+`])}},forms:{title:e=>{const{normalize:n}=e;return n(["Forms"])},name_label:e=>{const{normalize:n}=e;return n(["Name"])},name_placeholder:e=>{const{normalize:n}=e;return n(["Name"])},submit:e=>{const{normalize:n}=e;return n(["Say My Name"])},greeting:e=>{const{normalize:n,interpolate:o,named:r}=e;return n(["Hello, ",o(r("name")),"!"])}},modals:{title:e=>{const{normalize:n}=e;return n(["Modals"])},showDefault:e=>{const{normalize:n}=e;return n(["Default Modal"])},showCustom:e=>{const{normalize:n}=e;return n(["Custom Modal"])},defaultModalContent:e=>{const{normalize:n}=e;return n(["Default modals are quite modest."])},customModal:{title:e=>{const{normalize:n}=e;return n(["Custom Modal"])},content:e=>{const{normalize:n}=e;return n(["Custom modals can get as fancy as you want!"])},button:e=>{const{normalize:n}=e;return n(["Cool!"])}}},storage:{title:e=>{const{normalize:n}=e;return n(["Storage"])},taskName:e=>{const{normalize:n}=e;return n(["Task name"])},newTask:e=>{const{normalize:n}=e;return n(["New task"])},submit:e=>{const{normalize:n}=e;return n(["Add"])},deleteTask:e=>{const{normalize:n}=e;return n(["delete"])},deleteTask_a11y:e=>{const{normalize:n,interpolate:o,named:r}=e;return n(["Delete '",o(r("task")),"'"])}},ui:{close:e=>{const{normalize:n}=e;return n(["Close"])}},errors:{required:e=>{const{normalize:n}=e;return n(["This is required"])}}};export{t as default};
