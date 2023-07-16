261fe5095d25112682f9e343988122169cc873ab
Noel De Martin <noel@noeldemartin.com>
2023-07-16 17:36:34 +0900

    Set up global `$t` fallback

--- packages/core/src/bootstrap/index.ts
+++ packages/core/src/bootstrap/index.ts
@@ -4,6 +4,7 @@ import type { Component } from 'vue';
 import directives from '@/directives';
 import services from '@/services';
 import ui from '@/ui';
+import { translate } from '@/utils';
 import type { AerogelOptions } from '@/bootstrap/options';
 
 export async function bootstrapApplication(rootComponent: Component, options: AerogelOptions = {}): Promise<void> {
@@ -28,5 +29,6 @@ export async function bootstrapApplication(rootComponent: Component, options: Ae
 
     await Promise.all(plugins.map((plugin) => plugin.install(app, options)) ?? []);
 
+    app.config.globalProperties.$t ??= translate;
     app.mount('#app');
 }
