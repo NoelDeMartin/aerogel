d312110062afb7ff2defdc39be1089d34effd0e3
Noel De Martin <noel@noeldemartin.com>
2023-09-03 09:16:31 +0200

    Include soukai in new apps

--- packages/cli/templates/app/src/main.ts
+++ packages/cli/templates/app/src/main.ts
@@ -1,9 +1,13 @@
 import i18n from '@aerogel/plugin-i18n';
+import soukai from '@aerogel/plugin-soukai';
 import { bootstrapApplication } from '@aerogel/core';
 
 import './assets/styles.css';
 import App from './App.vue';
 
 bootstrapApplication(App, {
-    plugins: [i18n({ messages: import.meta.glob('@/lang/*.yaml') })],
+    plugins: [
+        i18n({ messages: import.meta.glob('@/lang/*.yaml') }),
+        soukai({ models: import.meta.glob('@/models/*', { eager: true }) }),
+    ],
 });
