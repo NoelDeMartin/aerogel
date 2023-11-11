41015155b5e91730bd0162304fb58fd115462f9c
Noel De Martin <noel@noeldemartin.com>
2023-11-11 13:50:23 +0100

    Fix waitForReload cypress step

--- packages/cypress/src/support/commands/app.ts
+++ packages/cypress/src/support/commands/app.ts
@@ -1,4 +1,4 @@
 export function waitForReload(): void {
-    cy.get('#app.loading').should('.exist');
+    cy.get('#app.loading').should('exist');
     cy.get('#app.loading').should('not.exist');
 }
