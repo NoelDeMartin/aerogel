f78de598acc35c7c4286710e3b8030beae71aafe
Noel De Martin <noel@noeldemartin.com>
2023-11-12 19:45:13 +0100

    Improve task logs

--- packages/cypress/src/tasks/solid-request.ts
+++ packages/cypress/src/tasks/solid-request.ts
@@ -4,12 +4,12 @@ import { authenticate } from './solid-utils';
 import { defineTask, log } from './utils';
 
 export default defineTask(async (...args: GetClosureArgs<typeof fetch>) => {
-    log(`Running request for ${args[0]}...`);
+    log(`${args[1]?.['method'] ?? 'GET'} ${args[0]}...`);
 
     const authenticatedFetch = await authenticate();
     const response = await authenticatedFetch(...args);
 
-    log(`Request completed with status ${response.status}.`);
+    log(`Response: ${response.status}`);
 
     return {
         headers: response.headers,
