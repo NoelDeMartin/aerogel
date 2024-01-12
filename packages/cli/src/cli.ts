702e17f12493b62feba1d0af54d7c3b1d07f934c
Noel De Martin <noel@noeldemartin.com>
2024-01-12 18:39:46 +0100

    Fix CLI version and name

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,6 +1,9 @@
 import { Command } from 'commander';
+import { facade, fail } from '@noeldemartin/utils';
+import { resolve } from 'path';
+import { existsSync, readFileSync } from 'fs';
+
 import { CreateCommand } from '@/commands/create';
-import { facade } from '@noeldemartin/utils';
 import { GenerateComponentCommand } from '@/commands/generate-component';
 import { GenerateModelCommand } from '@/commands/generate-model';
 import { GenerateOverridesCommand } from '@/commands/generate-overrides';
@@ -12,7 +15,7 @@ export class CLIService {
     public run(argv?: string[]): void {
         const program = new Command();
 
-        program.name('ag').description('AerogelJS CLI').version('0.0.0');
+        program.name('gel').description('AerogelJS CLI').version(this.getVersion());
 
         CreateCommand.define(program);
         GenerateComponentCommand.define(program);
@@ -22,6 +25,19 @@ export class CLIService {
         InstallCommand.define(program);
 
         program.parse(argv);
+    }
+
+    public getVersion(): string {
+        const errorMessage = 'Could not find CLI\'s version, please report this bug.';
+        const packageJsonPath = resolve(__dirname, '../package.json');
+
+        if (!existsSync(packageJsonPath)) {
+            throw new Error(errorMessage);
+        }
+
+        const packageJson = JSON.parse(readFileSync(packageJsonPath).toString()) as { version?: string };
+
+        return packageJson.version ?? fail(errorMessage);
     }
 
 }
