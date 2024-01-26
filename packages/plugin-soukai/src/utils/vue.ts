3e39cbcaebb0813ba1346d5332b0311d06bf2f7c
Noel De Martin <noel@noeldemartin.com>
2024-01-26 13:12:02 +0100

    Implement computedModel util

--- packages/plugin-soukai/src/utils/vue.ts
+++ packages/plugin-soukai/src/utils/vue.ts
@@ -1,8 +1,41 @@
-import { arrayRemove } from '@noeldemartin/utils';
+import { arrayRemove, tap } from '@noeldemartin/utils';
 import { onCleanMounted } from '@aerogel/core';
-import { onMounted, ref } from 'vue';
+import { customRef, onMounted, onScopeDispose, ref, watchEffect } from 'vue';
 import type { Model, ModelConstructor } from 'soukai';
-import type { Ref } from 'vue';
+import type { ComputedRef, Ref } from 'vue';
+
+export function computedModel<T extends Model | null | undefined>(compute: () => T): ComputedRef<T> {
+    return customRef((track, trigger) => {
+        let value: T;
+        const modelListeners: Array<() => void> = [];
+        const onModelUpdated = (model: Model) => model.id === value?.id && trigger();
+        const stopModelListeners = () => (
+            modelListeners.forEach((stop) => stop()), modelListeners.splice(0, modelListeners.length)
+        );
+
+        onScopeDispose(stopModelListeners);
+        watchEffect(() => {
+            const newValue = compute();
+
+            if (newValue?.static() !== value?.static()) {
+                stopModelListeners();
+            }
+
+            value = newValue;
+            trigger();
+
+            if (value) {
+                modelListeners.push(value.static().on('updated', onModelUpdated));
+                modelListeners.push(value.static().on('relation-loaded', onModelUpdated));
+            }
+        });
+
+        return {
+            get: () => tap(value, () => track()),
+            set: () => console.warn('Computed model ref was not set (they are immutable).'),
+        };
+    }) as ComputedRef<T>;
+}
 
 export function useModelCollection<T extends Model>(modelClass: ModelConstructor<T>): Ref<T[]> {
     const models = ref<T[]>([]) as Ref<T[]>;
