9f9564ab9f8da05f60d7868db361edbc5601ee39
Noel De Martin <noel@noeldemartin.com>
2024-06-08 10:10:19 +0200

    Implement AGSolidUserAvatar component

---
+++ packages/plugin-solid/src/components/AGSolidUserAvatar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="relative flex items-center justify-center rounded-full bg-gray-200">
+        <AGSolidImage
+            v-if="$solid.user?.avatarUrl"
+            :src="$solid.user?.avatarUrl"
+            alt=""
+            class="absolute inset-0 h-full w-full rounded-full"
+        />
+        <span class="text-sm font-semibold tracking-wider text-gray-500" aria-hidden="true">
+            {{ userInitials }}
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import Solid from '@/services/Solid';
+
+const userInitials = computed(() => {
+    const name = Solid.user?.name ?? '?';
+
+    return name
+        .split(' ')
+        .map((part) => part[0]?.toUpperCase())
+        .join('')
+        .slice(0, 3);
+});
+</script>
--- packages/plugin-solid/src/components/index.ts
+++ packages/plugin-solid/src/components/index.ts
@@ -1,5 +1,6 @@
 import AGSolidImage from './AGSolidImage.vue';
 import AGSolidStatus from './AGSolidStatus.vue';
+import AGSolidUserAvatar from './AGSolidUserAvatar.vue';
 import { IAGSolidStatusLoggedInSlotProps } from './AGSolidStatus';
 
-export { AGSolidImage, AGSolidStatus, IAGSolidStatusLoggedInSlotProps };
+export { AGSolidImage, AGSolidStatus, AGSolidUserAvatar, IAGSolidStatusLoggedInSlotProps };
