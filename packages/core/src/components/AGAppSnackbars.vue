ba892fd83c1a6d6e59b2c87c5f648799d49ba0fb
Noel De Martin <noel@noeldemartin.com>
2024-06-08 10:08:18 +0200

    Fix snackbars z-index

--- packages/core/src/components/AGAppSnackbars.vue
+++ packages/core/src/components/AGAppSnackbars.vue
@@ -1,5 +1,5 @@
 <template>
-    <div aria-live="assertive" class="z-60 pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:p-6">
+    <div aria-live="assertive" class="pointer-events-none fixed inset-0 z-50 flex items-end px-4 py-6 sm:p-6">
         <div class="flex w-full flex-col items-end space-y-4">
             <component
                 :is="snackbar.component"
