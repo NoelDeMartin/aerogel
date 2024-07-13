ec2b24cb7c63d11676ee791c9834c908c6229e5d
Noel De Martin <noel@noeldemartin.com>
2024-07-13 10:32:25 +0200

    Save development version of community-server

--- packages/cli/src/plugins/Solid.ts
+++ packages/cli/src/plugins/Solid.ts
@@ -27,7 +27,7 @@ export class Solid extends Plugin {
     protected async installNpmDependencies(): Promise<void> {
         await Shell.run('npm install soukai-solid@next --save-exact');
         await Shell.run('npm install @noeldemartin/solid-utils@next --save-exact');
-        await Shell.run('npm install @solid/community-server@7 --save');
+        await Shell.run('npm install @solid/community-server@~7.0 --save-dev');
         await super.installNpmDependencies();
     }
 
