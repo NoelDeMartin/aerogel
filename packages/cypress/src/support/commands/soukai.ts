ecf4c2737b43d20cfac283ce80ac5913de65f502
Noel De Martin <noel@noeldemartin.com>
2025-02-25 13:52:43 +0100

    Implement indexedDB cypress helpers

--- packages/cypress/src/support/commands/soukai.ts
+++ packages/cypress/src/support/commands/soukai.ts
@@ -1,5 +1,21 @@
+import { requireUrlParentDirectory } from '@noeldemartin/utils';
+import { openDB } from 'idb';
 import { IndexedDBEngine } from 'soukai';
+import type { JsonLD } from '@noeldemartin/solid-utils';
 import type { ModelConstructor, ModelsRegistry } from 'soukai';
+
+async function getIndexedDBObject<T = object>(database: string, store: string, id: string): Promise<T | null> {
+    try {
+        const db = await openDB(database);
+        const result = await db.get(store, id);
+
+        db.close();
+
+        return result ?? null;
+    } catch (error) {
+        return null;
+    }
+}
 
 export function model<T extends keyof ModelsRegistry>(name: T): Cypress.Chainable<ModelsRegistry[T]>;
 export function model<T extends ModelConstructor = ModelConstructor>(name: string): Cypress.Chainable<T>;
@@ -9,4 +25,10 @@ export function model(name: string): Cypress.Chainable<ModelConstructor> {
 
 export function soukaiReset(): void {
     new IndexedDBEngine().purgeDatabase();
+}
+
+export function indexedDBDocument(id: string): Cypress.Chainable<JsonLD | null> {
+    return Cypress.Promise.cast(
+        getIndexedDBObject<JsonLD>('soukai', requireUrlParentDirectory(id), id),
+    ) as unknown as Cypress.Chainable<JsonLD | null>;
 }
