883688b71831164897856b300dfe05e1d10ddcdf
Noel De Martin <noel@noeldemartin.com>
2025-03-11 11:28:38 +0100

    Export markdown utils

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './composition/events';
 export * from './composition/forms';
 export * from './composition/hooks';
 export * from './composition/persistent';
+export * from './markdown';
 export * from './tailwindcss';
 export * from './vue';
