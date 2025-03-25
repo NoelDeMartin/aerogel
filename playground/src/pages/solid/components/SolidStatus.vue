a78d6b339b0c814d36bd3113d584f4f350974774
Noel De Martin <noel@noeldemartin.com>
2025-03-25 17:47:36 +0100

    Fix CI, take 3

---
+++ playground/src/pages/solid/components/SolidStatus.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <AGMarkdown
+            :text="
+                $t('solid.welcome', {
+                    name: $solid.user?.name ?? $solid.user?.webId,
+                    url: $solid.user?.webId,
+                })
+            "
+        />
+
+        <BaseButton
+            small
+            color="clear"
+            class="mt-1"
+            @click="$solid.logout()"
+        >
+            {{ $t('solid.logout') }}
+            <i-mdi-logout class="h-4 w-4" />
+        </BaseButton>
+    </div>
+</template>
