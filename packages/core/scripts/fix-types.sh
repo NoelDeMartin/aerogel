4d012e739f05a2cd730645e3b6a12757ad8ca775
Noel De Martin <noel@noeldemartin.com>
2025-04-07 10:53:42 +0200

    Fix build

--- packages/core/scripts/fix-types.sh
+++ packages/core/scripts/fix-types.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sed -i 's/declare module '"'"'@aerogel\/core\/[^'"'"']*'"'"'/declare module '"'"'@aerogel\/core'"'"'/g' dist/aerogel-core.d.ts
+sed -i 's/$nextTick: nextTick;/$nextTick: typeof nextTick;/g' dist/aerogel-core.d.ts
