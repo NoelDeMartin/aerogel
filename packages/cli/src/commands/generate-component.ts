e3690204fb6f5fb723525710ac64e3b3fe4fd487
Noel De Martin <noel@noeldemartin.com>
2025-04-09 12:24:31 +0200

    Fix generate component command

--- packages/cli/src/commands/generate-component.ts
+++ packages/cli/src/commands/generate-component.ts
@@ -1,4 +1,4 @@
-import { arrayFilter, arrayFrom, stringToSlug } from '@noeldemartin/utils';
+import { arrayFrom, stringToSlug } from '@noeldemartin/utils';
 import { Node, SyntaxKind } from 'ts-morph';
 import type { ArrayLiteralExpression, CallExpression, SourceFile } from 'ts-morph';
 
@@ -9,7 +9,6 @@ import Template from '@aerogel/cli/lib/Template';
 import { app } from '@aerogel/cli/lib/utils/app';
 import { editFiles, findDescendant } from '@aerogel/cli/lib/utils/edit';
 import { templatePath } from '@aerogel/cli/lib/utils/paths';
-import type { CommandOptions } from '@aerogel/cli/commands/Command';
 
 export interface Options {
     button?: boolean;
@@ -26,52 +25,21 @@ export class GenerateComponentCommand extends Command {
         ['path', 'Component path (relative to components folder; extension not necessary)'],
     ];
 
-    protected static override options: CommandOptions = {
-        button: {
-            description: 'Create a custom button',
-            type: 'boolean',
-        },
-        checkbox: {
-            description: 'Create a custom checkbox',
-            type: 'boolean',
-        },
-        input: {
-            description: 'Create a custom input',
-            type: 'boolean',
-        },
-        story: {
-            description: 'Create component story using Histoire',
-            type: 'boolean',
-        },
-    };
-
     private path: string;
-    private options: Options;
 
-    constructor(path: string, options: Options = {}) {
+    constructor(path: string) {
         super();
 
         this.path = path;
-        this.options = options;
-    }
-
-    protected override async validate(): Promise<void> {
-        const components = arrayFilter([this.options.button, this.options.input, this.options.checkbox]).length;
-
-        if (components > 1) {
-            Log.fail('Can only use one of \'button\', \'input\', or \'checkbox\' flags!');
-        }
     }
 
     protected override async run(): Promise<void> {
         this.assertAerogelOrDirectory('src/components');
-        this.assertHistoireInstalled();
 
         const files = new Set<string>();
         const [directoryName, componentName] = this.parsePathComponents();
 
         await this.createComponent(directoryName, componentName, files);
-        await this.createStory(directoryName, componentName, files);
         await this.declareComponents();
 
         const filesList = arrayFrom(files)
@@ -81,33 +49,13 @@ export class GenerateComponentCommand extends Command {
         Log.info(`${componentName} component created successfully! The following files were created:\n\n${filesList}`);
     }
 
-    protected assertHistoireInstalled(): void {
-        if (!this.options.story) {
-            return;
-        }
-
-        if (!File.contains('package.json', '"histoire"') && !File.contains('package.json', '"@aerogel/histoire"')) {
-            Log.fail(`
-                Histoire is not installed yet! You can install it running:
-                npx gel install histoire
-            `);
-        }
-    }
-
     protected async createComponent(directoryName: string, componentName: string, files: Set<string>): Promise<void> {
         await Log.animate('Creating component', async () => {
             if (File.exists(`src/components/${this.path}.vue`)) {
                 Log.fail(`${this.path} component already exists!`);
             }
 
-            const templateName = this.options.input
-                ? 'component-input'
-                : this.options.button
-                    ? 'component-button'
-                    : this.options.checkbox
-                        ? 'component-checkbox'
-                        : 'component';
-            const componentFiles = Template.instantiate(templatePath(templateName), `src/components/${directoryName}`, {
+            const componentFiles = Template.instantiate(templatePath('component'), `src/components/${directoryName}`, {
                 component: {
                     name: componentName,
                     slug: stringToSlug(componentName),
@@ -115,30 +63,6 @@ export class GenerateComponentCommand extends Command {
             });
 
             componentFiles.forEach((file) => files.add(file));
-        });
-    }
-
-    protected async createStory(directoryName: string, componentName: string, files: Set<string>): Promise<void> {
-        if (!this.options.story) {
-            return;
-        }
-
-        await Log.animate('Creating story', async () => {
-            const templateName = this.options.input
-                ? 'component-input-story'
-                : this.options.button
-                    ? 'component-button-story'
-                    : this.options.checkbox
-                        ? 'component-checkbox-story'
-                        : 'component-story';
-            const storyFiles = Template.instantiate(templatePath(templateName), `src/components/${directoryName}`, {
-                component: {
-                    name: componentName,
-                    slug: stringToSlug(componentName),
-                },
-            });
-
-            storyFiles.forEach((file) => files.add(file));
         });
     }
 
