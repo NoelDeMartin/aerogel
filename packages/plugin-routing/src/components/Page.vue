c564a418ff1834eb5c5e24a2936d06dead58aad2
Noel De Martin <noel@noeldemartin.com>
2025-05-01 08:16:14 +0200

    Tweak page styles

--- packages/plugin-routing/src/components/Page.vue
+++ packages/plugin-routing/src/components/Page.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="mx-auto flex w-full max-w-screen-md grow flex-col items-center px-4 py-8 text-center md:px-0">
+    <main class="mx-auto flex w-full max-w-screen-md grow flex-col items-center px-4 py-8 md:px-0">
         <slot />
     </main>
 </template>
