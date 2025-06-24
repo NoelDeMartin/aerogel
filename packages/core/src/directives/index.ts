d86959f8ee7dcf2194447eb424c1095912a5e5f8
Noel De Martin <noel@noeldemartin.com>
2025-06-24 12:44:24 +0200

    Fix directive types

--- packages/core/src/directives/index.ts
+++ packages/core/src/directives/index.ts
@@ -5,22 +5,24 @@ import { definePlugin } from '@aerogel/core/plugins';
 import measure from './measure';
 import safeHtml from './safe-html';
 
-const builtInDirectives: Record<string, Directive> = {
+export const aerogelDirectives = {
     'measure': measure,
     'safe-html': safeHtml,
-};
+} as const satisfies Record<string, Directive>;
+
+export type AerogelDirectives = typeof aerogelDirectives;
 
 export * from './measure';
 
 export default definePlugin({
     install(app, options) {
         const directives = {
-            ...builtInDirectives,
+            ...aerogelDirectives,
             ...options.directives,
         };
 
         for (const [name, directive] of Object.entries(directives)) {
-            app.directive(name, directive);
+            app.directive(name, directive as Directive);
         }
     },
 });
@@ -32,7 +34,5 @@ declare module '@aerogel/core/bootstrap/options' {
 }
 
 declare module 'vue' {
-    interface ComponentCustomDirectives {
-        measure: Directive<string, string>;
-    }
+    interface ComponentCustomDirectives extends AerogelDirectives {}
 }
