e1d6631cae817abf4b3700e5ea3cd4a6cb3949ad
Noel De Martin <noel@noeldemartin.com>
2025-10-09 20:11:16 +0200

    Tweak details

--- packages/core/src/components/ui/Details.vue
+++ packages/core/src/components/ui/Details.vue
@@ -1,10 +1,10 @@
 <template>
     <details class="group">
-        <summary
-            class="-ml-2 flex w-[max-content] items-center rounded-lg py-2 pr-3 pl-1 hover:bg-gray-100 focus-visible:outline focus-visible:outline-gray-700"
-        >
+        <summary :class="renderedSummaryClasses">
             <IconCheveronRight class="size-6 transition-transform group-open:rotate-90" />
-            <span>{{ label }}</span>
+            <slot name="label">
+                <span>{{ label ?? $td('ui.details', 'Details') }}</span>
+            </slot>
         </summary>
 
         <div :class="renderedContentClasses">
@@ -14,10 +14,20 @@
 </template>
 
 <script setup lang="ts">
+import IconCheveronRight from '~icons/zondicons/cheveron-right';
 import { classes } from '@aerogel/core/utils';
 import { type HTMLAttributes, computed } from 'vue';
-import IconCheveronRight from '~icons/zondicons/cheveron-right';
 
-const { label, contentClass } = defineProps<{ label: string; contentClass?: HTMLAttributes['class'] }>();
+const {
+    label = undefined,
+    contentClass,
+    summaryClass,
+} = defineProps<{ label?: string; contentClass?: HTMLAttributes['class']; summaryClass?: HTMLAttributes['class'] }>();
 const renderedContentClasses = computed(() => classes('pt-2 pl-4', contentClass));
+const renderedSummaryClasses = computed(() =>
+    classes(
+        '-ml-2 flex w-[max-content] items-center rounded-lg py-2 pr-3 pl-1',
+        'hover:bg-gray-100 focus-visible:outline focus-visible:outline-gray-700',
+        summaryClass,
+    ));
 </script>
