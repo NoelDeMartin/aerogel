47ee6e5e3485291a8763a804016f87a051476893
Noel De Martin <noel@noeldemartin.com>
2025-10-09 20:11:29 +0200

    Create primary-text color

--- packages/core/src/components/AppLayout.vue
+++ packages/core/src/components/AppLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex min-h-full flex-col text-base leading-tight font-normal text-gray-900 antialiased">
+    <div class="text-primary-text flex min-h-full flex-col text-base leading-tight font-normal antialiased">
         <slot v-if="$errors.hasStartupErrors" name="startup-crash">
             <component :is="$ui.requireComponent('startup-crash')" />
         </slot>
