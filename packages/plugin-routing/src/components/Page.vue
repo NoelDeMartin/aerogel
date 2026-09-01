4509ec861d0ed18c9490637620878cb5191fd1fd
Noel De Martin <noel@noeldemartin.com>
2026-09-01 08:36:55 +0200

    Add title to Page component

--- packages/plugin-routing/src/components/Page.vue
+++ packages/plugin-routing/src/components/Page.vue
@@ -1,5 +1,8 @@
 <template>
     <main :class="renderedRootClasses">
+        <PageTitle v-if="title">
+            {{ title }}
+        </PageTitle>
         <slot />
     </main>
 </template>
@@ -9,7 +12,7 @@ import { computed } from 'vue';
 import { classes } from '@aerogel/core';
 import type { HTMLAttributes } from 'vue';
 
-const { class: rootClasses } = defineProps<{ class?: HTMLAttributes['class'] }>();
+const { class: rootClasses } = defineProps<{ class?: HTMLAttributes['class']; title?: string }>();
 
 const renderedRootClasses = computed(() =>
     classes('mx-auto flex w-full max-w-screen-content grow flex-col px-edge py-8', rootClasses));
---
+++ packages/plugin-routing/src/components/PageTitle.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-xl font-semibold text-slate-900">
+        <slot />
+    </h1>
+</template>
